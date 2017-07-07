7d519d7e56fb4c92a93fef3e0eba0126706af421
Tyson Gern <tgern@pivotal.io>
2017-07-07 16:24:52 -0600

    Add security

--- applications/timesheets-server/src/main/java/io/pivotal/pal/tracker/timesheets/App.java
+++ applications/timesheets-server/src/main/java/io/pivotal/pal/tracker/timesheets/App.java
@@ -7,13 +7,19 @@ import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableOAuth2Client;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.web.client.RestOperations;
 
 import java.util.TimeZone;
 
 
+@EnableWebSecurity
+@EnableResourceServer
 @EnableCircuitBreaker
 @EnableEurekaClient
+@EnableOAuth2Client
 @SpringBootApplication
 @ComponentScan({"io.pivotal.pal.tracker.timesheets", "io.pivotal.pal.tracker.restsupport"})
 public class App {
