00f53530be8f5c1cd263690ef02f1d1f6b167091
kelseyp1993 <kelsey.pritsker@gmail.com>
2019-05-01 14:41:28 -0700

    SSO Security Implementation

--- components/rest-support/src/main/java/io/pivotal/pal/tracker/restsupport/RestConfig.java
+++ components/rest-support/src/main/java/io/pivotal/pal/tracker/restsupport/RestConfig.java
@@ -5,18 +5,11 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.client.RestOperations;
 import org.springframework.web.client.RestTemplate;
 
 
 @Configuration
 public class RestConfig {
-
-    @LoadBalanced
-    @Bean
-    public RestOperations restOperations() {
-        return new RestTemplate();
-    }
 
     @Bean
     public ObjectMapper objectMapper() {
