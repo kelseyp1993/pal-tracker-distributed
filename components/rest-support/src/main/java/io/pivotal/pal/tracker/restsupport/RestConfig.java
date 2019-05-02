8fb5df4a3ebc3d2dbb8f34ccf7b375cd70a84a9b
kelseyp1993 <kelsey.pritsker@gmail.com>
2019-05-01 17:00:19 -0700

    Oauth repaired RestConfig

--- components/rest-support/src/main/java/io/pivotal/pal/tracker/restsupport/RestConfig.java
+++ components/rest-support/src/main/java/io/pivotal/pal/tracker/restsupport/RestConfig.java
@@ -5,11 +5,17 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestOperations;
 import org.springframework.web.client.RestTemplate;
 
 
 @Configuration
 public class RestConfig {
+
+    @LoadBalanced
+    public RestOperations restOperations() {
+        return new RestTemplate();
+    }
 
     @Bean
     public ObjectMapper objectMapper() {
